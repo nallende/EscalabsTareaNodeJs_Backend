5a4075318ea9f707a83e77a32a91866e33cdcace
nallende <nallende@neosoporte.cl>
2021-12-16 13:19:10 -0300

    Commit Jueves

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const router = express.Router();
-import { showMessage } from "../controllers/auth"
+import { showMessage, registro } from "../controllers/auth"
 
 router.get("/:message", showMessage);
+router.post('/register', registro)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-//const connectDB = require("./database");
+
 import cors from 'cors';
 import { readdirSync } from 'fs';
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 import mongoose from 'mongoose';
 const swaggerUI = require("swagger-ui-express");
@@ -15,8 +15,9 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
 
-//db coonection
+
 // db connection
 mongoose
     .connect(process.env.DATABASE, {
@@ -33,10 +34,8 @@ app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
-//route:option
-/*app.use('/api', userRoutes );*/
 
-//routes Opcion 2 con filesyn es lo que se esta usando
+//routes 
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 const port = process.env.PORT || 8000;
